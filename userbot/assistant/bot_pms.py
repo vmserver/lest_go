c4237d7fc33e76c991b795c8b6d802de0909e7cf
your-enemy <90089871+your-enemy@users.noreply.github.com>
2021-11-08 22:09:18 +0530

    Update bot_pms.py

--- userbot/assistant/bot_pms.py
+++ userbot/assistant/bot_pms.py
@@ -103,17 +103,12 @@ async def bot_start(event):
                 my_mention=my_mention,
             )
         else:
-            start_msg = f"Hey! 👤{mention},\
+            start_msg = f"Hey! {mention},\
                         \nI am {my_mention}'s assistant bot.\
-                        \nYou can contact to my master from here.\
-                        \n\nPowered by [Catuserbot](https://t.me/catuserbot)"
+                        \nYou can contact to my master from here."
         buttons = [
             (
-                Button.url("Repo", "https://github.com/sandy1709/catuserbot"),
-                Button.url(
-                    "Deploy",
-                    "https://dashboard.heroku.com/new?button-url=https%3A%2F%2Fgithub.com%2FMr-confused%2Fcatpack&template=https%3A%2F%2Fgithub.com%2FMr-confused%2Fcatpack",
-                ),
+                Button.url("𝘑𝘰𝘪𝘯 𝘗𝘳𝘦𝘥𝘢𝘵𝘰𝘳'𝘴 𝘈𝘣𝘰𝘥𝘦", "https://t.me/Predators_Abode"),
             )
         ]
     else:
