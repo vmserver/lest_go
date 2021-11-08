f39ff191a207a88bceb00ec18fc6e2c2e1cd6657
your-enemy <90089871+your-enemy@users.noreply.github.com>
2021-11-08 23:52:26 +0530

    Update startup.py

--- userbot/utils/startup.py
+++ userbot/utils/startup.py
@@ -63,7 +63,7 @@ async def startupmessage():
         if BOTLOG:
             Config.CATUBLOGO = await catub.tgbot.send_file(
                 BOTLOG_CHATID,
-                "https://telegra.ph/file/4e3ba8e8f7e535d5a2abe.jpg",
+                "https://telegra.ph/file/bef93bfccee4be8c46650.jpg",
                 caption="**Your CatUserbot has been started successfully.**",
                 buttons=[(Button.url("Support", "https://t.me/catuserbot"),)],
             )
